95054a5b26758aeaefa45a17c02d82873c84eb9d
Silvio Silva <smsilva@gmail.com>
2024-11-06 07:53:53 -0300

    feat: initial config

---
+++ src/variables.tf
@@ -0,0 +1,29 @@
+variable "common_name" {
+  description = "The common name to use for the ACME certificate"
+  type        = string
+  default     = "echo.sandbox.wasp.silvios.me"
+}
+
+variable "subject_alternative_names" {
+  description = "The subject alternative names to use for the ACME certificate"
+  type        = list(string)
+  default     = ["echo.sandbox.wasp.silvios.me"]
+}
+
+variable "dns_provider" {
+  description = "The DNS provider to use for the ACME challenge"
+  type        = string
+  default     = "azuredns"
+}
+
+variable "acme_registration_email_address" {
+  description = "The email address to use for the ACME registration"
+  type        = string
+  default     = "letsencrypt@silvios.me"
+}
+
+variable "acme_server_url" {
+  description = "The URL of the ACME server to use for the Let's Encrypt service"
+  type        = string
+  default     = "https://acme-staging-v02.api.letsencrypt.org/directory"
+}
